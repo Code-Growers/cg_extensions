d411238bcc8d17274a52b88fa9b607ac63869de3
adamkvasnicka <adam.kvasnicka@generaliceska.cz>
2021-01-08 19:08:20 +0100

    UPDATE: added route observer to flutter_toast_provider

--- lib/src/providers/flutter_toast_provider.dart
+++ lib/src/providers/flutter_toast_provider.dart
@@ -2,6 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
+final RouteObserver<PageRoute<dynamic>> flutterToastRouteObserver =
+    RouteObserver<PageRoute<dynamic>>();
+
 class FlutterToastProvider extends StatefulWidget {
   final Widget child;
 
@@ -11,10 +14,37 @@ class FlutterToastProvider extends StatefulWidget {
   _FlutterToastProviderState createState() => _FlutterToastProviderState();
 }
 
-class _FlutterToastProviderState extends State<FlutterToastProvider> {
+class _FlutterToastProviderState extends State<FlutterToastProvider>
+    with RouteAware {
+  @override
+  void initState() {
+    super.initState();
+    _initCurrentToastContext();
+  }
+
+  void _initCurrentToastContext() {
+    FToast().init(context);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    flutterToastRouteObserver.subscribe(this, ModalRoute.of(context));
+  }
+
+  @override
+  void didPopNext() {
+    _initCurrentToastContext();
+  }
+
   @override
   Widget build(BuildContext context) {
-    FToast().init(context);
     return widget.child;
+  }
+
+  @override
+  void dispose() {
+    flutterToastRouteObserver.unsubscribe(this);
+    super.dispose();
   }
 }
