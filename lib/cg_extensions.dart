a5d85dbf3da9d36af0433959eedbe74363879ba9
adamkvasnicka <adam.kvasnicka@generaliceska.cz>
2021-04-18 09:29:57 +0200

    FIX: fixed name clash using only partially exported dart_extensions extension

--- lib/cg_extensions.dart
+++ lib/cg_extensions.dart
@@ -1,6 +1,13 @@
 library cg_extensions;
 
-export 'package:dart_extensions/dart_extensions.dart';
+export 'package:dart_extensions/src/flutter/context.dart';
+export 'package:dart_extensions/src/flutter/container.dart';
+export 'package:dart_extensions/src/flutter/center.dart';
+export 'package:dart_extensions/src/flutter/align.dart';
+export 'package:dart_extensions/src/flutter/padding.dart';
+export 'package:dart_extensions/src/flutter/sized_box.dart';
+export 'package:dart_extensions/src/flutter/text.dart';
+export 'package:dart_extensions/src/flutter/gesture_detector.dart';
 export 'package:dartx/dartx.dart';
 export 'package:febu/febu.dart';
 
