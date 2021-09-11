412c3929bd4adf8f4ad894c0177452bc6effcea3
adamkvasnicka <adam.kvasnicka@generaliceska.cz>
2021-09-11 19:45:32 +0200

    bumped flutter bloc

--- lib/src/text_style_extension.dart
+++ lib/src/text_style_extension.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-extension CGTextStyleExtension on TextStyle {
-  TextStyle get scale => copyWith(fontSize: fontSize?.sp);
+extension CGTextStyleExtension on TextStyle? {
+  TextStyle? get scale =>
+      this != null ? this!.copyWith(fontSize: this!.fontSize?.sp) : this;
 }
